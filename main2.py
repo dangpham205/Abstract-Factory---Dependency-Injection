1c8135f659c511afaf0b3a1735735b87759ce016
Dawnny Pham <dangbnp@tradeline.vn>
2022-11-08 17:30:37 +0700

    push

---
+++ main2.py
@@ -0,0 +1,44 @@
+import abc
+
+from pythondi import Provider, configure, configure_after_clear, inject
+
+
+class Repo:
+    """Interface class"""
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def get(self):
+        pass
+
+
+class SQLRepo(Repo):
+    """Impl class"""
+    def get(self):
+        print('SQLRepo')
+
+
+class Usecase:
+    @inject()
+    def __init__(self, repo: Repo):
+        self.repo = repo
+
+
+if __name__ == '__main__':
+    # Init provider
+    provider = Provider()
+
+    # Bind `Impl` class to `Interface` class
+    provider.bind(Repo, SQLRepo)
+    provider.bind(Usecase, SQLRepo)
+
+    # Inject with configure
+    configure(provider=provider)
+
+    # Or if you want to fresh injection, use `configure_after_clear`
+    configure_after_clear(provider=provider)
+
+    # Init class without arguments
+    u = Usecase()
+    # data = u
+    print(u.get())
